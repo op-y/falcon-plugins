fcd3d8336b7fbf1cfd3cde23aad2488019b3031e
yezhiqin <yezhiqin@jenkins-salve.xh.bj.gozap.com>
2016-11-11 17:52:20 +0800

    bugfix

--- 60_request.py
+++ 60_request.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python
 #-*- coding:utf-8 -*-
+#
+# Author: yezhiqin@hualala.com
+# Date  : 2016/11/11
 
 import os
 import json
@@ -10,58 +13,67 @@ class RequestPatrol:
     def __init__(self, config_file):
         self.config_file = config_file
         with open(self.config_file) as file:
+            self.data = []
             config = json.load(file)
-            self.endpoint = config['endpoint']
+            self.endpoint = os.uname()[1]
             self.step = config['step']
             self.counterType = config['counterType']
             self.tags = config['tags']
             self.request = config['request']
 
+    # function to dispatch checking items.
     def HandleRequest(self):
         for item in self.request:
             metric = item['metric']
             protocol = item['protocol']
             type = item['type']
             url = item.get('url','')
+            method = item.get('method','get')
             ip = item.get('ip','')
             port = item.get('port','')
             ping = item.get('ping','')
             pong = item.get('pong','')
             if protocol == 'http':
                 value = self.CheckHttp(type, url, ping, pong)
-                self.PushPoint(self.endpoint, metric, self.step, value, self.counterType, self.tags)
+                self.AddPoint(self.endpoint, metric, self.step, value, self.counterType, self.tags)
             elif protocol == 'tcp':
                 value = self.CheckTcp(type, ip, port, ping, pong)
             elif protocol == 'udp':
                 value = self.CheckUdp(type, ip, port, ping, pong)
-            else:pass
+            else:
+                return -1
+        self.PushData()
     
-    def CheckHttp(self, type, url, ping="", pong=""):
+    # function to check HTTP status.
+    def CheckHttp(self, type, url, method="", ping="", pong=""):
         try:
-            response = requests.post(url)
             if type == "status":
+                response = requests.request(method, url)
                 code = response.status_code
                 if code == 200:
-                    return 1
-                else:
                     return 0
+                else:
+                    return 1
             elif type == "semanteme":
+                response = requests.request(method, url)
                 content = response.text
                 if pong in content:
-                    return 1
-                else:
                     return 0
+                else:
+                    return 1
             else:
                 return -1
         except:
             return -1
 
+    # TODO:: function to check TCP port status.
     def CheckTcp(self, type, ip, port, ping="", pong=""):pass
 
+    # TODO:: function to check UDP port status.
     def CheckUdp(self, type, ip, port, ping="", pong=""):pass
 
-    def PushPoint(self, endpoint, metric, step, value, counterType, tags):
-        point = [{
+    def AddPoint(self, endpoint, metric, step, value, counterType, tags):
+        point = {
                     'endpoint': endpoint,
                     'metric': metric,
                     'timestamp': int(time.time()),
@@ -69,10 +81,14 @@ class RequestPatrol:
                     'value': value,
                     'counterType': counterType,
                     'tags': tags
-                }]
-        print(json.dumps(point))
+                }
+        self.data.append(point)
 
+    def PushData(self):
+        print(json.dumps(self.data))
+
+# start to check
 path = os.path.split(os.path.realpath(__file__))[0]
-cfg = path+"/request_patrol.json"
+cfg = path+"/request.json"
 patrol = RequestPatrol(cfg)
 patrol.HandleRequest();
